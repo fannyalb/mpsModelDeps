f70a58f2c5b53c17aaa28811aa71fb6a71ce100e
Fanny Albrecht <fanny.albrecht@aon.at>
2022-10-04 16:46:59 +0200

    first working example

---
+++ drawDeps.py
@@ -0,0 +1,62 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+def drawDeps(model_deps):
+    G = nx.DiGraph()
+    node_colors = dict()
+    edge_colors = []
+    i = 1
+    for model in model_deps.keys():
+        G.add_node(model)
+        node_colors[model] = i
+        i += 1
+
+    for model in model_deps.keys():
+        for dep in model_deps[model]:
+            G.add_edge(model, dep)
+            edge_colors.append(node_colors[model])
+            if dep not in model_deps.keys():
+                node_colors[dep] = 0
+    node_colors_list = list(node_colors.values())
+
+    d = dict(G.out_degree())
+    colormap = plt.cm.gist_rainbow
+    fig = plt.figure(1, figsize=(80, 80))
+    node_sizes = [(v*50) for v in d.values()]
+    # coord = nx.kamada_kawai_layout(G)
+    # coord = nx.shell_layout(G, rotate=1.5)
+    # coord = nx.circular_layout(G)
+    # coord = nx.spring_layout(G, k=1)
+    coord = arc_layout(G.nodes())
+    nx.draw_networkx_nodes(G, coord,
+                           node_color=node_colors_list,
+                           cmap=colormap,
+                           node_size=node_sizes
+                           )
+    nx.draw_networkx_edges(G,
+                           coord,
+                           arrowstyle='->',
+                           edge_color=edge_colors,
+                           edge_cmap=colormap,
+                           connectionstyle="arc3,rad=1.5",
+                           arrowsize=14
+                           )
+    labels = nx.draw_networkx_labels(G,
+                                     coord,
+                                     font_size=10)
+    for _, l in labels.items():
+        l.set_rotation(45)
+
+    fig.tight_layout()
+    plt.show()
+
+
+def arc_layout(nodes):
+    coord = dict()
+    i = 0
+    for node in nodes:
+        nodename = node
+        coord[nodename] = [i, 0]
+        i = i+3
+    return coord
---
+++ modelDeps.py
@@ -0,0 +1,164 @@
+#!/bin/python3
+import pathlib
+import os
+import re
+import glob
+import argparse
+from drawDeps import drawDeps
+
+project_path = ""
+
+ignorelist = ["test", "org.modellwerkstatt"]
+
+sortweights = dict()
+sortweights[".unit."] = 10
+sortweights[".domain."] = 20
+sortweights[".extern."] = 30
+sortweights[".tecinfra."] = 40
+
+
+def isToIgnore(name):
+    for substring in ignorelist:
+        if substring in name:
+            return True
+    return False
+
+
+def sortweightsPrinted():
+    output = ''
+    for substring in sortweights:
+        output += f'\t"{substring}" = {sortweights[substring]}\n'
+    output += '\t DEFAULT = 50'
+    return output
+
+
+def get_mps_files(inDir):
+    print(f'Get mspfiles from {inDir}')
+    if not inDir.is_dir():
+        print(f'{inDir} is not a Dir!')
+        exit
+    # files = glob.glob(os.path.join(inDir, '*.mps'), recursive=True)
+    files = pathlib.Path(inDir).rglob('*.mps')
+    print(files)
+    return files
+
+
+def find_model_deps(path):
+    dep_pattern = "<import index.*\(([\w\.]*)\).* />$"
+    model_pattern = "<model.*\((.*)\).*>$"
+    model_deps = dict()
+    mpsfiles = get_mps_files(path)
+    for filepath in mpsfiles:
+        filename = os.path.basename(filepath)
+        deps = set()
+        mpsfile = open(filepath, "r")
+        model = filename[:-4]
+        for line in mpsfile:
+            depmatch = re.search(dep_pattern, line)
+            modelmatch = re.search(model_pattern, line)
+            if depmatch and depmatch.group(1):
+                deps.add(depmatch.group(1))
+            if modelmatch and modelmatch.group(1):
+                model = modelmatch.group(1)
+        model_deps[model] = deps
+    return model_deps
+
+
+def filterModelDeps(model_deps):
+    filtered_deps = dict()
+    for model in model_deps:
+        if isToIgnore(model):
+            continue
+        deps = set()
+        for dep in model_deps[model]:
+            if isToIgnore(dep):
+                continue
+            deps.add(dep)
+        filtered_deps[model] = deps
+    return filtered_deps
+
+
+def printModelDepsAsString(model_deps):
+    for model in model_deps:
+        print(f'Model {model} depends on:')
+        for dep in model_deps[model]:
+            print(f'\t{dep}')
+
+
+def init_argparse() -> argparse.ArgumentParser:
+    sortweightsprint = sortweightsPrinted()
+    epilog_text = f'''
+        current sortweights:
+        {sortweightsprint}
+        current ignorelist:
+            {ignorelist}'''
+    parser = argparse.ArgumentParser(prog='modelDeps',
+                                     formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     epilog=epilog_text)
+    parser.add_argument('--path', nargs=1,
+                        required=True,
+                        type=pathlib.Path,
+                        help='path to MPS-models')
+    parser.add_argument('--ignore', nargs='*',
+                        help='list of (sub)strings to ignore (models or deps)'
+                        )
+    parser.add_argument('--sortby', nargs='*',
+                        metavar="KEY=VALUE",
+                        help='list of (substrings) and weigths to sort by.  \
+                                e.g. "inout=10"')
+    parser.add_argument('--clearsort',
+                        action="store_true",
+                        help='Clear current sortweights')
+    return parser
+
+
+def parseArguments(parser):
+    args = parser.parse_args()
+    if args.path:
+        project_path = args.path[0]
+        print(f'Project path now: {project_path}')
+    if args.ignore:
+        for x in args.ignore:
+            print(f'Adding "{x}" to Ignorelist')
+            ignorelist.append(x)
+    if args.clearsort:
+        print('Deleting default sortweights....')
+        sortweights.clear()
+    if args.sortby:
+        for kv in args.sortby:
+            items = kv.split('=')
+            key = items[0].strip()
+            if len(items) > 1 and items[1].isdigit():
+                value = items[1]
+                print(f'Add "{key}={value}" to sortweights for sorting')
+                sortweights[key] = int(value)
+
+    return project_path
+
+
+def sortFunc(modelname):
+    categories = list(sortweights.keys())
+    for category in reversed(categories):
+        if category in modelname:
+            return sortweights[category]
+    return 50
+
+
+def sortModelDeps(model_deps):
+    sorted_nodes = dict(sorted(model_deps.items(),
+                               key=lambda x: sortFunc(x[0])))
+    return sorted_nodes
+
+
+def main():
+    parser = init_argparse()
+    project_path = parseArguments(parser)
+    model_deps = find_model_deps(project_path)
+    filtered_deps = filterModelDeps(model_deps)
+    sorted_deps = sortModelDeps(filtered_deps)
+
+    drawDeps(sorted_deps)
+
+
+if __name__ == "__main__":
+     main()
